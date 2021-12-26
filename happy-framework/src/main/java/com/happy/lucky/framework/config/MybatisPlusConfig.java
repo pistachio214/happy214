5922546c6c068a707c643becb0b9aefd22692c38
Songyang <aileshang0226@163.com>
2021-12-26 13:28:27 +0800

    update mybatis plus 升级至 3.4.2

--- happy-framework/src/main/java/com/happy/lucky/framework/config/MybatisPlusConfig.java
+++ happy-framework/src/main/java/com/happy/lucky/framework/config/MybatisPlusConfig.java
@@ -1,6 +1,8 @@
 package com.happy.lucky.framework.config;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +12,10 @@ public class MybatisPlusConfig {
      * 分页插件
      */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+    public MybatisPlusInterceptor paginationInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+
+        return mybatisPlusInterceptor;
     }
 }
