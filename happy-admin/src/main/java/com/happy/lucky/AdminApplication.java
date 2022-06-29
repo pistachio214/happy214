126ca89a43b46010700eefcc2e0a066479ec1b7e
Songyang <aileshang0226@163.com>
2022-06-30 02:34:51 +0800

    update 后台API启动引导说明

--- happy-admin/src/main/java/com/happy/lucky/AdminApplication.java
+++ happy-admin/src/main/java/com/happy/lucky/AdminApplication.java
@@ -4,6 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 后台管理系统 Api 启动引导
+ *
+ * @author songyangpeng
+ */
 @MapperScan(basePackages = {"com.happy.lucky.system.mapper"})
 @SpringBootApplication
 public class AdminApplication {
